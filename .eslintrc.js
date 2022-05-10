97d29971673e538635b890a1eaf874755db35475
climacot <fermt111@gmail.com>
2022-05-09 21:55:25 -0500

    configuracion del linter y prettier para mantener el estilo de codigo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,36 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-use-before-define': 'off'
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
-  ],
-};
+        'no-undef': 'off'
+      }
+    }
+  ]
+}
