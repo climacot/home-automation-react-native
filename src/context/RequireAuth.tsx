3403d29996a293f08257cb6ba48ffc46557160fe
climacot <fermt111@gmail.com>
2022-06-15 10:44:25 -0500

    refactor componente require auth

--- src/context/RequireAuth.tsx
+++ src/context/RequireAuth.tsx
@@ -1,18 +1,17 @@
 import { Navigate, useLocation } from 'react-router-native'
-import React from 'react'
+import { View } from 'react-native'
+import React, { ReactNode } from 'react'
 import useAuth from '../hooks/useAuth'
 
-export default function RequireAuth({ children }: { children: JSX.Element }) {
-  const auth = useAuth()
+type ComponentProps = {
+  children: ReactNode
+}
+
+export default function RequireAuth({ children }: ComponentProps) {
   const location = useLocation()
+  const { session } = useAuth()
 
-  if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
-    return <Navigate to="/" state={{ from: location }} replace />
-  }
+  if (!session) return <Navigate to="/" state={{ from: location }} replace />
 
-  return children
+  return <View>{children}</View>
 }
