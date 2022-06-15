7c33fa59e4371740fd5025dbd686bd947cbae91a
climacot <fermt111@gmail.com>
2022-06-15 10:43:48 -0500

    refactor context init

--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -1,3 +1,10 @@
 import { createContext } from 'react'
+import { Isession } from '../Models/user'
 
-export const AuthContext = createContext({})
+interface AuthInterface {
+  session: Isession | undefined | null
+  logInWithEmailAndPassword: any
+  signOut: any
+}
+
+export const AuthContext = createContext<AuthInterface>(null!)
