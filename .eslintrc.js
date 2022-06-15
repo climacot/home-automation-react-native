b0111fe3408ce2a6009c0bf1b11f981ec7a8d8ad
climacot <fermt111@gmail.com>
2022-06-15 10:41:25 -0500

    no-unused-vars configurado a warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,23 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
+  extends: ['plugin:react/recommended', 'standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'no-use-before-define': 'off',
     'comma-dangle': 'off',
     'space-before-function-paren': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
   },
   overrides: [
     {
@@ -32,8 +26,9 @@ module.exports = {
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
-        'no-undef': 'off'
-      }
-    }
-  ]
+        'no-undef': 'off',
+        'no-unused-vars': 'warn',
+      },
+    },
+  ],
 }
