49fb27b9954fa5fbff2ceac427a171da404424a5
climacot <fermt111@gmail.com>
2022-06-15 17:13:49 -0500

    delete unused comments lines

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -29,37 +29,3 @@ const style = StyleSheet.create({
     width: '100%',
   },
 })
-
-// const clear = () => {
-//   setAdress('')
-//   setDisplayName('')
-//   setEmail('')
-//   setId('')
-//   setPassword('')
-//   setPhoneNumber('')
-// }
-
-// const handleSubmit = async () => {
-//   if (email.length > 0 && password.length > 0 && displayName.length > 0 && id.length > 0 && phoneNumber.length > 0) {
-//     try {
-//       const authPromise = await auth().createUserWithEmailAndPassword(email, password)
-//       const reference = await database().ref(`/usuarios/${authPromise.user.uid}`)
-//       reference.set({
-//         cargo: 'usuario',
-//         correo: email,
-//         direccion: adress,
-//         foto: '',
-//         identificacion: id,
-//         nombre: displayName,
-//         telefono: phoneNumber,
-//       })
-
-//       setError('Usuario creado con exito')
-//       clear()
-//     } catch (errorHandler: any) {
-//       setError(errorHandler.toString())
-//     }
-//   } else {
-//     setError('Todos los campos son obligatorios')
-//   }
-// }
