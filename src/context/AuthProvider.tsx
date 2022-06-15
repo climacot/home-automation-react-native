156e08433413f5f9776d075bb1cd0f3d045b07bf
climacot <fermt111@gmail.com>
2022-06-15 10:44:00 -0500

    refactor componente provider auth

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,91 +1,70 @@
 import { AuthContext } from './AuthContext'
-import React, { ReactNode, useState } from 'react'
+import { errorFirebaseAuth } from '../utils/errorsFirebase'
+import { getData, storeData } from '../utils/asyncStorage'
+import { Icredentials, Isession } from '../Models/user'
 import auth from '@react-native-firebase/auth'
+import database from '@react-native-firebase/database'
+import React, { ReactNode, useEffect, useState } from 'react'
 
 type ComponentProps = {
   children: ReactNode
 }
-interface credentials {
-  username: string
-  password: string
-}
-
-interface Isession {
-  displayName: string | null
-  email: string | null
-  phoneNumber: string | null
-  uid: string
-  photoURL: string | null
-}
 
 export default function AuthProvider({ children }: ComponentProps) {
-  const [session, setSession] = useState<Isession>()
+  const [session, setSession] = useState<Isession | null>()
 
-  const logInWithEmailAndPassword = async (credentials: credentials) => {
+  useEffect(() => {
+    getData().then(sessionStore => {
+      if (!sessionStore) {
+        setSession(null)
+        return
+      }
+
+      setSession(sessionStore)
+    })
+  }, [])
+
+  const logInWithEmailAndPassword = async (credentials: Icredentials, onLoader: any, onError: any) => {
     try {
       const { username, password } = credentials
       const credential = await auth().signInWithEmailAndPassword(username, password)
+      onLoader(true)
       const { user } = credential
       const { displayName, email, phoneNumber, uid, photoURL } = user
+      const db = await database().ref(`usuarios/${uid}`).once('value')
+      const { cargo, nombre, foto, identificacion } = db.val()
+      onLoader(false)
 
-      setSession({
-        displayName,
+      const userSession = {
+        displayName: nombre,
         email,
+        id: identificacion,
         phoneNumber,
+        photoURL: foto,
+        role: cargo,
         uid,
-        photoURL,
-      })
+      }
+
+      setSession(userSession)
+      storeData(userSession)
     } catch (error) {
-      console.log(error)
-      setSession(undefined)
+      errorFirebaseAuth(error, onError)
+      onLoader(false)
+      setSession(null)
+      storeData(null)
     }
   }
 
-  return <AuthContext.Provider value={{ session, logInWithEmailAndPassword }}>{children}</AuthContext.Provider>
+  const signOut = async () => {
+    try {
+      await auth().signOut()
+      setSession(null)
+      storeData(null)
+    } catch (error) {
+      setSession(null)
+      storeData(null)
+    }
+  }
+
+  return <AuthContext.Provider value={{ session, logInWithEmailAndPassword, signOut }}>{children}</AuthContext.Provider>
 }
-
-// const storeData = async (value: IUserInfo | null) => {
-//   try {
-//     const jsonValue = JSON.stringify(value)
-//     await AsyncStorage.setItem('@statususer', jsonValue)
-//   } catch (e) {}
-// }
-
-// const getData = async () => {
-//   try {
-//     const jsonValue = await AsyncStorage.getItem('@statususer')
-//     return jsonValue != null ? JSON.parse(jsonValue) : null
-//   } catch (e) {}
-// }
-
-// const signIn = async (emailp: string, passwordp: string, callback: Function, callbackError: Function) => {
-//   try {
-//     const credential = await auth().signInWithEmailAndPassword(emailp, passwordp)
-//     const userCredential = credential
-//     const { uid, email } = userCredential.user
-//     const db = await database().ref(`usuarios/${uid}`).once('value')
-//     const { nombre, identificacion, foto, cargo, telefono } = db.val()
-
-//     const userInfo = {
-//       displayName: nombre,
-//       email,
-//       id: identificacion,
-//       phoneNumber: telefono,
-//       photoURL: foto,
-//       rol: cargo,
-//       uid,
-//     }
-
-//     setUser(userInfo)
-//     storeData(userInfo)
-
-//     // cargo === 'administrador' ? navigate('/admin', { replace: true }) : navigate('/user', { replace: true })
-
-//     callback()
-
-//     return cargo
-//   } catch (error) {
-//     callbackError()
-//     return null
-//   }
-// }
