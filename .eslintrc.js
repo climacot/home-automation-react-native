c613075e040f7f70762d305f41ef433a1126e32a
climacot <fermt111@gmail.com>
2022-06-22 22:59:37 -0500

    linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'space-before-function-paren': 'off'
+  }
+}
--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import {Text, View} from 'react-native';
-import React from 'react';
+import { Text, View } from 'react-native'
+import React from 'react'
 
 export default function App() {
   return (
     <View>
       <Text>Reat native!</Text>
     </View>
-  );
+  )
 }
